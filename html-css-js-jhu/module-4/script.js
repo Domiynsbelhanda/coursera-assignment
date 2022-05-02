3e7c5722193edf22fae7ba244cca115ae5e3991d
Dominique Youness <youness.dominique@gmail.com>
2022-05-02 11:58:13 +0200

    update name

--- html-css-js-jhu/module-4/script.js
+++ html-css-js-jhu/module-4/script.js
@@ -1,11 +1,8 @@
 (function (window) {
     
-    var names = ["Dominique", "Youness", "Francel", "Hadry", "Rollins", "Marvin", "LaPaille", "Belhanda", "Jin Cool", "The Best"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i in names) {
-    
-    
       var firstLetter = ((names[i]).charAt(0)).toLowerCase();
-
       if (firstLetter == 'j') {
          window.byeSpeaker.speak(names[i]);
       } else {
